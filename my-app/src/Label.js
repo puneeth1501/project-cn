6ec90a2ef21d69d07987c94c8750226d3769c6ff
Sai Joshika Gummireddy <saijoshikareddy@gmail.com>
2024-11-09 23:40:13 -0600

    Commit

---
+++ my-app/src/Label.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Label = ({ label }) => {
+    return (
+        <>
+            <h2 className='label'>{label}</h2>
+        </>
+    );
+};
+
+export default Label;
