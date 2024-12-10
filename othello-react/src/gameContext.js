23f3b7969268ac4a2a9afe921db0f7da8cc62e72
ANANTHA PUNEETH CHOWDARY <96817637+puneeth1501@users.noreply.github.com>
2024-12-10 00:07:46 -0600

    Update gameContext.js

--- othello-react/src/gameContext.js
+++ othello-react/src/gameContext.js
@@ -1,26 +1,4 @@
-// import React from 'react';
 
-// const defaultState = {
-//     isInRoom: false,
-//     setInRoom: () => {},
-//     playerColor: 1,
-//     setPlayerColor: () => {},
-//     isPlayerTurn: false,
-//     setPlayerTurn: () => {},
-//     isGameStarted: false,
-//     setGameStarted: () => {},
-//     isGameFinished: false,
-//     setGameFinished: () => {},
-//     roomName: '',
-//     setRoomName: () => {},
-//     roomList: [''],
-//     setRoomList: () => {}
-// };
-
-// export default React.createContext(defaultState);
-//bottom imp
-
-// src/gameContext.js
 import React from 'react';
 
 export default React.createContext({
