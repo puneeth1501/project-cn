417db7f57a08372ac3989b7018fa800083c97e44
ANANTHA PUNEETH CHOWDARY <96817637+puneeth1501@users.noreply.github.com>
2024-12-10 00:09:41 -0600

    Update app.js

--- othello-backend-test/src/app.js
+++ othello-backend-test/src/app.js
@@ -1,20 +1,4 @@
-// const express = require('express');
-// const cors = require('cors');
 
-// const app = express();
-
-// app.use(cors({
-//     origin: ['https://othello-react.netlify.app', 'http://localhost:3000']
-// }));
-
-// app.use(express.json());
-
-// // Basic health check route
-// app.get('/', (req, res) => {
-//     res.json({ status: 'Othello Game Server Running' });
-// });
-
-// module.exports = app;
 
 const express = require('express');
 const app = express();
