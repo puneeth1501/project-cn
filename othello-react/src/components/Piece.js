3ed95e8f9ecae4cb88f488ed9a9e41effd55ca15
ANANTHA PUNEETH CHOWDARY <96817637+puneeth1501@users.noreply.github.com>
2024-12-10 01:38:31 -0600

    Update Piece.js

--- othello-react/src/components/Piece.js
+++ othello-react/src/components/Piece.js
@@ -1,17 +1,17 @@
-import React from 'react';
+// import React from 'react';
 
-const Piece = ({ color }) => {
-    return (
-        <>
-            {color === 0 ? (
-                <div className='hint-disk'></div>
-            ) : color === 1 ? (
-                <div className='black-disk'></div>
-            ) : (
-                <div className='white-disk'></div>
-            )}
-        </>
-    );
-};
+// const Piece = ({ color }) => {
+//     return (
+//         <>
+//             {color === 0 ? (
+//                 <div className='hint-disk'></div>
+//             ) : color === 1 ? (
+//                 <div className='black-disk'></div>
+//             ) : (
+//                 <div className='white-disk'></div>
+//             )}
+//         </>
+//     );
+// };
 
-export default Piece;
+// export default Piece;
