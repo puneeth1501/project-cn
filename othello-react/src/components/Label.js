282c42d64b6ec0a0ff51be02ae8a310eba8efddc
ANANTHA PUNEETH CHOWDARY <96817637+puneeth1501@users.noreply.github.com>
2024-12-10 01:38:10 -0600

    Update Label.js

--- othello-react/src/components/Label.js
+++ othello-react/src/components/Label.js
@@ -1,3 +1,14 @@
+// import React from 'react';
+
+// const Label = ({ label }) => {
+//     return (
+//         <>
+//             <h2 className='label'>{label}</h2>
+//         </>
+//     );
+// };
+
+// export default Label;
 import React from 'react';
 
 const Label = ({ label }) => {
@@ -8,4 +19,18 @@ const Label = ({ label }) => {
     );
 };
 
-export default Label;
+const Piece = ({ color }) => {
+    return (
+        <>
+            {color === 0 ? (
+                <div className='hint-disk'></div>
+            ) : color === 1 ? (
+                <div className='black-disk'></div>
+            ) : (
+                <div className='white-disk'></div>
+            )}
+        </>
+    );
+};
+
+export { Label as default, Piece };
